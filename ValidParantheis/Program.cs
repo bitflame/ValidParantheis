bd7e46200edd471495066edb84f8186b80e34e0c
Liquid Falme <sansari1@cox.net>
2019-10-20 16:04:58 -0400

    Successfully submited to LeetCode.

---
+++ ValidParantheis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParantheis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParanthesis("()[]{}"));
+            Console.ReadLine();
+        }
+        private static Boolean ValidParanthesis(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in s)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    stack.Push(ch);
+                }
+                else
+                {
+                    if (stack.Count() != 0 && IsPairParanthesis(stack.Peek(), ch))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return (stack.Count() == 0);
+        }
+        private static Boolean IsPairParanthesis(char left, char right)
+        {
+            return (left == '(' && right == ')' || left == '[' && right == ']' || left == '{' && right == '}') ;
+        }
+    }
+}
